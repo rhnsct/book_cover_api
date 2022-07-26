4ba77c2fa5f8559fe492f256dc4d32542478b435
rhnsct <rhnsct@gmail.com>
2022-07-27 01:23:50 +0200

    origins

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ origins = ["http://localhost",
            "http://localhost:5500",
            "https://localhost:5500",
            "http://127.0.0.1:5500",
+           "http://127.0.0.1:5500/",
            "https://127.0.0.1:5500"
            ]
 
