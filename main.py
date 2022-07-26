86b5e3991f59c6cc77debae7052569b8c2cf6512
rhnsct <rhnsct@gmail.com>
2022-07-27 01:12:32 +0200

    cors changes

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from urllib.request import urlopen
 import requests
 
 app = FastAPI()
+
+origins = ["http://localhost",
+           "http://localhost:8080",
+           "http://localhost:5500",
+           "https://localhost:5500",
+           "http://127.0.0.1:5500",
+           "https://127.0.0.1:5500"
+           ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def google_api_call(title, author):
